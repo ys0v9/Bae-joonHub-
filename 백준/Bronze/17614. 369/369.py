e5b8354698d9f66e8873edc55d4d0d0b692248ee
최수아 <sua9097@gmail.com>
2025-08-07 22:05:58 +0900

    [Bronze III] Title: 369, Time: 672 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17614. 369/369.py
+++ 백준/Bronze/17614. 369/369.py
@@ -1,8 +1,12 @@
-n = int(input())
+def count_369(n):
+    cnt = 0
+    for i in range(1, n + 1):
+        x = i
+        while x > 0:
+            if x % 10 in [3, 6, 9]:
+                cnt += 1
+            x //= 10
+    return cnt
 
-cnt = 0 
-for i in range(1, n+1):
-    for j in str(i):
-        if j == '3' or j == '6' or j == '9':
-            cnt += 1
-print(cnt)
+n = int(input())
+print(count_369(n))
